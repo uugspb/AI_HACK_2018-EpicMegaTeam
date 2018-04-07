2b6b46c067693845f8f39ba492808d38f46a61af
askochnev <andrey.s.kochnev@gmail.com>
2018-04-07 22:35:25 +0300

    ML agent WIP

---
+++ Assets/Scripts/ML/SpaceRocksAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceRocksAgent : Agent {
+
+    private void Start()
+    {
+        
+    }
+
+    public override void CollectObservations()
+    {
+        foreach (var ship in GameContext.Instance.ships)
+        {
+            AddVectorObs(ship.acceleration);
+            AddVectorObs(ship.health);
+            AddVectorObs(ship.ownerName.GetHashCode());
+            AddVectorObs(ship.position);
+            AddVectorObs(ship.rotation);
+            AddVectorObs(ship.acceleration);
+            AddVectorObs((int)ship.weapon);
+            AddVectorObs(ship.health);
+        }
+
+        foreach (var projectile in GameContext.Instance.projectiles)
+        {
+            AddVectorObs(projectile.GetVelocity());
+            AddVectorObs(projectile.GetOwnerName().GetHashCode());
+        }
+
+        foreach(var bonus in GameContext.Instance.bonuses)
+        {
+            AddVectorObs((int)bonus.GetBonusType());
+            AddVectorObs(bonus.transform.position);
+        }
+    }
+
+    public override void AgentReset()
+    {
+    }
+}
