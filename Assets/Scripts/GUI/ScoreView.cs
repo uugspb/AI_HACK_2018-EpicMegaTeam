bd2ea973e0fa52097f4640626c3c03f36f629f4e
askochnev <andrey.s.kochnev@gmail.com>
2018-04-08 10:27:16 +0300

    Bots scores

---
+++ Assets/Scripts/GUI/ScoreView.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreView : MonoBehaviour {
+    [SerializeField] string x;
+    [SerializeField] TMPro.TextMeshProUGUI tex;
+	
+	// Update is called once per frame
+	void Update () {
+        tex.text = ScoreCounter.score.Find(d => d.player == x).score.ToString();
+	}
+}
